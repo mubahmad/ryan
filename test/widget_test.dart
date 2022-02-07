ee3fc907d5c56b2c6a004bea8a55dedbeadcca68
mubahmad <mubaidinahmad@gmail.com>
2022-02-07 16:53:11 +0200

    fixed names

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ryan/main.dart';
+import 'package:Ryan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
